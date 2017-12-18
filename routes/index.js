2c2bef71764ebbdfa5807357fcbaeffa81100d84
rmoayinla <rmoayinla@yahoo.com>
2017-12-18 08:59:36 +0100

    added a new todo GET route

--- routes/index.js
+++ routes/index.js
@@ -40,6 +40,8 @@ exports = module.exports = function (app) {
 	app.get('/gallery', routes.views.gallery);
 	app.all('/contact', routes.views.contact);
 
+	app.get('/todos', routes.views.todo.index);
+
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
 
