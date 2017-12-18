464135816245d0124d3cb21f5bd9993b034c0e40
rmoayinla <rmoayinla@yahoo.com>
2017-12-18 10:02:38 +0100

    streamlined the options for setting keystone options only, moved the error handlers to errorhandler.js

--- libs/options.js
+++ libs/options.js
@@ -3,6 +3,7 @@ var path = require('path');
 var keystoneNodeMailer = require('keystone-nodemailer');
 
 module.exports = function(keystone){
+    
     //set the base url for the current project //
     //sample baseUrl is http://host:8080/blog/ or https://host:4234/blog
     var setBaseUrl = function(){
@@ -21,47 +22,7 @@ module.exports = function(keystone){
     //add the baseUrl in keystone options table //
     keystone.set('base url', setBaseUrl());
 
-    //set an error handler for server 500 errors //
-    keystone.set('500', function(err, req, res, next){
-        var dashes = '\n------------------------------------------------\n';
-        var locals, errMessage;
-
-        locals = res.locals;
-        res.status(500);
-        //if we are not in production, add detailed error message //
-        if(keystone.get('env') !== "production"){
-            locals.error = err;
-        }
-        if(keystone.get('logger')){
-            if (err instanceof Error) {
-				console.log((err.type ? err.type + ' ' : '') + 'Error thrown for request: ' + req.url);
-			} else {
-				console.log('Error thrown for request: ' + req.url);
-			}
-			console.log(err.stack || err);
-        }
-        //return json data if the accept header is json //
-        if (req.headers.accept === 'application/json') {
-            return res.json({ 
-                error: 'Internal Server error',
-                message: err.message || "",
-                stack: err.stack || {}, 
-                details: err
-             });
-        }
-        //render a 500 template page for server errors //
-        res.render('errors/500', locals);
-        
-    });
-
-    keystone.set('404', function(req,res,next){
-        var locals;
-        res.status(404);
-        locals = res.locals;
-        locals.url = req.originalUrl;
-        //render a 400 template page, this is found in templates/views/errors/400.pug //
-        res.render('errors/404',locals);
-    });
+    
     //set the default email transport method //
     keystone.set('email transport', 'nodemailer');
 
