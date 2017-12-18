0fc646cb2be03bf77115f822ab6c43fa03464e31
rmoayinla <rmoayinla@yahoo.com>
2017-12-18 08:59:17 +0100

    Todo model, this file creates a new collection in the db "todo" and specifies path and schemas for the model

---
+++ models/Todo.js
@@ -0,0 +1,35 @@
+var
+   keystone = require('keystone'),
+   Types = keystone.Field.Types,
+   path = require('path'),
+   schema,
+   Todo;
+
+Todo = new keystone.List('Todo', {
+    autokey: {from: 'title', path: 'slug',},
+    searchFields: 'title',
+});
+
+var myStorage = new keystone.Storage({
+    adapter: keystone.Storage.Adapters.FS,
+    fs: {
+      path: '../public/uploads',
+      publicPath: '/files',
+    },
+});
+
+Todo.add({
+    title: {type:String, initial: true, default:'', required: true,},
+    image: {type: Types.File, storage: myStorage},
+    createdAt: {type: Date, default: Date.now},
+    status: {type: Types.Select, options: ['In progress', 'Finished', 'On hold', 'New']},
+    createdBy: {type: Types.Relationship, ref:'Y',},
+    updatedAt: {type: Date, default: Date.now},
+    description: {type: Types.Html, wysiwyg: true},
+    tags:{type: Types.Select, options:['career','family','work','relationship','sport']},
+    category:{type: Types.Relationship, ref: 'PostCategory', many: true},
+});
+
+Todo.register();
+
+
