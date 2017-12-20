e23134ba5f3beca01620aeba1af5c8cbb96fd154
rmoayinla <rmoayinla@yahoo.com>
2017-12-20 09:57:40 +0100

    still has work to do here, needs to handle the post action and create a new user, nothing is happening for now

--- routes/views/auth/join.js
+++ routes/views/auth/join.js
@@ -7,8 +7,21 @@ exports = module.exports = function(req, res){
     locals = res.locals;
     userModel = keystone.list('Y');
     locals.section = 'sign-up';
+
+    view.on('init', function(next){
+        locals.form = req.body || {};
+        next();
+    });
+
     view.on('post', function(next){
-        locals.form = req.body;
+        var userData;
+        userData = req.body;
+    });
+
+    view.on('get', function(next){
+        //redirect user to the admin page if we have someone logged in//
+        if(req.user) res.redirect('/keystone');
+        next();
     });
 
     //render the signup page containg the signup form in template/views/auth/signup.pug //
