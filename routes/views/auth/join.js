bd5787379bf1074c1c87b6b6a088774e1c386926
rmoayinla <rmoayinla@yahoo.com>
2017-12-22 14:55:07 +0100

    now using promises instead of async, still have some finalisation to do

--- routes/views/auth/join.js
+++ routes/views/auth/join.js
@@ -1,24 +1,30 @@
+require('es6-promise').polyfill();
 var 
     keystone = require('keystone'),
-    async = require('async');
+    async = require('async'),
+    extend = require('extend');
 
 exports = module.exports = function(req, res){
-    var view, locals,userModel;
+    var view, locals,userModel, formData, formErrors;
     view = new keystone.View(req, res);
     locals = res.locals;
     userModel = keystone.list('Y');
     //mark the navbar link active with ".active" class, @see templates/partials/navbar.pug//
     locals.section = 'sign-up';
 
+    formData = {username:'', firstname: '', lastname: '', email: '', password:'',};
+    formErrors = formData;
+
     view.on('init', function(next){
-        locals.form = req.body || {};
+        locals.form = extend({}, formData, req.body);
+        locals.error = formErrors;
         next();
     });
 
     view.on('post', function(next){
         var userData;
         userData = req.body;
-        async.series([
+        /*async.series([
 			
 			function(cb) {
 				
@@ -90,18 +96,74 @@ exports = module.exports = function(req, res){
             }
 			
 			var onSuccess = function() {
- 					res.redirect('/keystone');
+ 				res.redirect('/keystone');
 			}
 			
 			var onFail = function(e) {
                 req.flash('error', 'There was a problem signing you up, please try again.');
                 locals.form = userData;
-				return next();
+                console.log('Error creating user, error:%s', e);
+				next();
 			}
 			
 			keystone.session.signin({ email: req.body.email, password: req.body.password }, req, res, onSuccess, onFail);
 			
-		});
+        });*/
+        checkFormData(userData)
+            .then(function checkUsernameExists(data){
+                return new Promise(function(resolve, reject){
+                    keystone.list('Y').model.findOne({ username: data.username }, function(err, user) {
+                    
+                        if (err || user) {
+                            reject(new Error('User already exists with that Username.'), err);
+                        }
+                        
+                        resolve(data);
+                        
+                    });
+                });
+            })
+            .then(function checkEmailExists(data){
+                return new Promise(function(resolve, reject){
+                    keystone.list('Y').model.findOne({ email: data.email }, function(err, user) {
+                    
+                        if (err || user) {
+                            reject(new Error('User already exists with that email address.'));
+                        }
+                        
+                        resolve(data);
+                        
+                    });
+                });
+            })
+            .then(function createUser(data){
+                return new Promise(function(resolve, reject){
+                    var newUserData = {
+                        username: data.username,
+                        name: {
+                            first: data.firstname,
+                            last: data.lastname,
+                        },
+                        email: data.email,
+                        password: data.password
+                    };
+                    
+                    var User = keystone.list('Y').model,
+                        newUser = new User(newUserData);
+                    
+                    newUser.save(function(err) {
+                        if(err) reject(new Error('Error creating new user'), err );
+                        resolve("success");
+                    });     
+                    
+                });
+            })
+            .catch(function handleError(message, err){
+                req.flash('error', message);
+                if(err) console.log('User could not be created: %o', err);
+                next();
+            });
+        
     });
 
     view.on('get', function(next){
@@ -112,4 +174,22 @@ exports = module.exports = function(req, res){
 
     //render the signup page containing the signup form in template/views/auth/signup.pug //
     view.render('auth/signup');
+
+    function checkFormData(data){
+        return new Promise(function(resolve, reject){
+            if (!data.username || !data.firstname || !data.lastname || !data.email || !data.password) {
+                reject( new Error( 'Please enter a username, your name, email and password.'), true);
+            }
+            resolve(data);
+        });
+    }
+    
+
+    
+
+    
+
+ 
+
 };
+
